33b23f86a0939d743225ffb806b79d306beca3c1
palak-240 <palakjaswani24@gmail.com>
2025-11-30 10:21:59 +0530

    Add stack class with O(1) getMin functionality
    Implemented a stack class with push, pop, show, and getMin methods.

--- assignment-3/add2.cpp
+++ assignment-3/add2.cpp
@@ -1,5 +1,59 @@
-// additional question 2
 
-#include<iostream>
-
+//a stack that supports getMin() in O(1) time and O(1) extra space
+#include <iostream>
 using namespace std;
+
+class stack{
+    int top=-1;
+    int *arr;
+    int size;
+    public:
+    stack(int s){
+       size=s;
+       arr=new int[size];
+    }
+    void push(int);
+    void pop();
+    void show();
+    void getMin();
+};
+
+void stack::push(int item){
+    if(top==size-1){
+        cout<<"Stack is overflow";
+    }
+    else
+    top++;
+    arr[top]=item;
+}
+void stack::pop(){
+    if(top==-1){
+        cout<<"Stack is underflow";
+    }
+    else
+    top--;
+}
+void stack::show(){
+    int i;
+    for(i=top;i>=0;i--){
+        cout<<arr[i]<<" ";
+    }
+}
+//its O(n) 
+void stack::getMin(){
+    int min=arr[top];
+    for(int i=top;i>0;i--){
+        if(min>arr[i-1]){
+            min=arr[i-1];
+        }
+    }
+    cout<<min;
+}
+int main() {
+    stack s(5);
+    s.push(3);
+    s.push(2);
+    s.push(4);
+    s.getMin();
+    return 0;
+}
